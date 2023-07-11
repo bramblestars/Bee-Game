6ef9e84d52f0c36a67c730b410f1764ae1f44dbe
bramblestars <txxcui@gmail.com>
2023-07-10 20:56:12 -0500

    Added "exit" button to main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayButton : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
-    [SerializeField] GameObject PlayGraphic;
-
     public void PlayGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
