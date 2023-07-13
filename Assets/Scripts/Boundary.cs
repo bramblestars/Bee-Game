d5873e14047c7fdef173b333c365a672d86d477b
bramblestars <txxcui@gmail.com>
2023-07-13 02:16:57 -0500

    Added bee graphic to "You Win" screen with simple up-down animation and changed "You can't go any farther" message to reposition based on which boundary is being hit

--- Assets/Scripts/Boundary.cs
+++ Assets/Scripts/Boundary.cs
@@ -7,11 +7,16 @@ public class Boundary : MonoBehaviour
 {
 
     [SerializeField] private Image cantGoFartherMessage;
+    [SerializeField] private float horizontalOffset = 140f;
+    [SerializeField] private float verticalOffset = 90f;
+
     private bool timerRunning;
     private double timer;
+    RectTransform rectTransform;
 
     void Start() {
         cantGoFartherMessage.CrossFadeAlpha(0, 0f, false);
+        rectTransform = cantGoFartherMessage.GetComponent<RectTransform>();
     }
 
     void Update() {
@@ -26,9 +31,25 @@ public class Boundary : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) {
         cantGoFartherMessage.CrossFadeAlpha(1, 1f, false);
+        
+        switch(gameObject.tag) {
+            case "BoundaryRight":
+                rectTransform.localPosition = new Vector3(horizontalOffset, 0, 0);
+                break;
+            case "BoundaryLeft":
+                rectTransform.localPosition = new Vector3(-horizontalOffset, 0, 0);
+                break;
+            case "BoundaryTop":
+                rectTransform.localPosition = new Vector3(0, verticalOffset, 0);
+                break;
+            case "BoundaryBottom":
+                rectTransform.localPosition = new Vector3(0, -verticalOffset, 0);
+                break;
+        }
     }
 
     void OnTriggerExit2D(Collider2D other) {
+
         timerRunning = true; 
     }
 
